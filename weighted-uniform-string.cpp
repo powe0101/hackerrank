2a0819cd9717ab7849992665b2d8de8e7d8e0abe
Hyunjin Gil <powe0101@naver.com>
2019-05-21 16:46:13 +0900

    Update weighted-uniform-string.cpp

--- weighted-uniform-string.cpp
+++ weighted-uniform-string.cpp
@@ -1,27 +1,99 @@
-//https://www.hackerrank.com/challenges/weighted-uniform-string/
 //Created by G(powe0101@naver.com)
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
+//https://www.hackerrank.com/challenges/weighted-uniform-string
+//2가지 방식의 풀이 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void GetWeights(char alphabet, const string & s, vector<int>& weights)
+{
+    for (unsigned count = 1, i = 0; i < s.size(); ++i) 
+    {
+        const char ch = s[i];
+
+        if (ch == alphabet) 
+        {   
+            weights.push_back((alphabet - 96) * count++); 
+        }
+        else
+        {
+            count = 1;
+        }
+    }
+}
+
+vector<string> FindQueriesByWeights(const vector<int>& queries, const vector<int>& weights)
+{
+    vector<string> result;
+
+    for (int i = 0; i < queries.size(); ++i) {
+      auto pos = find(weights.begin(), weights.end(), queries[i]);
+      if (pos == weights.end())
+        result.push_back("No");
+      else
+        result.push_back("Yes");
+    }
+
+    return result;
+}
+
+// Complete the weightedUniformStrings function below.
+vector<string> weightedUniformStrings(const string& s, const vector<int>& queries) 
+{
+    vector<int> weights;
+
+    for(auto alphabet = 'a'; alphabet <= 'z'; ++alphabet)
+    {
+        GetWeights(alphabet, s, weights);
+    }
+
+    auto result = FindQueriesByWeights(queries, weights);
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int queries_count;
+    cin >> queries_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        int queries_item;
+        cin >> queries_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        queries[i] = queries_item;
+    }
+
+    vector<string> result = weightedUniformStrings(s, queries);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+
+
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -33,32 +105,33 @@ int main(){
     int n;
     cin >> n;
     
-    map<int,int> Queries;
+    map<int,int> list;
     for(int i = 0; i < alphabets.size(); ++i)
     {
         char ch = alphabets[i];
+        int uniform = (ch-96);
         int query = 0;
 
-        for(int pos = 0; pos < s.size(); ++pos)
+        for(int pos = 0; pos < s.size(); pos++)
         {
             char targetCh = s[pos];
             if(ch == targetCh){
-                query += 1;
-                Queries[query * (i+1)] = 1;
+                query += uniform;
+                list[query] = 1;
             }
-            else if(query > 0){
-                query = 1;
-                Queries[query * (i+1)] = 1;
-                break;
+            else
+            {
+                query = 0;
             }
         }
+        
     }
     
     for(int a0 = 0; a0 < n; a0++){
         int x;
         cin >> x;
         
-        if(Queries[x])
+        if(list[x])
             cout << "Yes" << endl;
         else
             cout << "No" << endl;
