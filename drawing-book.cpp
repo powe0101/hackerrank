6f16ad593ebbbeab574e1d34a5fd172fa323fe8e
powe0101 <hyunjin.direct@gmail.com>
2020-06-23 10:40:45 +0900

    Create drawing-book.cpp

---
+++ drawing-book.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the pageCount function below.
+ */
+int pageCount(int n, int p) 
+{
+    int temp = 0;
+    for(int i = 1; i <= n; i += 2)
+    {
+        if(i >= p)
+            break;
+        temp += 1;
+    }
+
+    int result = 0;
+    for(int i = n; i > 0; i -=2)
+    {
+        if(i-1 <= p && p <= i+1)
+        {
+            if(n == p){
+                result = 0;
+                break;
+            }
+            else if(n % 2 == 0 && i > n-1){
+                result = 1;
+                break;
+            }
+            break;
+        }
+        result += 1;
+    }
+
+    result = min(result, temp);
+
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
